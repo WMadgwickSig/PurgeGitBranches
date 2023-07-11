fac7aee08ea1db9f601f8104e64cd8f93f5b496d
WMadgwickSig <william.madgwick@signify.co.za>
2023-07-11 13:17:28 +0200

    Setting up basic console application

---
+++ GithubAction/ActionInputs.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using System;
+
+namespace GithubAction;
+
+public class ActionInputs
+{
+    [Option('t', "repoToken", Required = true, HelpText = "Github repository token used for authentication.")]
+    public string RepoToken { get; set; } = null!;
+
+    [Option('d', "dryRun", Required = true, HelpText = "Do a dry or test run first to see which branches will be purged?")]
+    public bool DryRun { get; set; }
+
+    static void ParseAndAssign(string? value, Action<string> assign)
+    {
+        if (value is { Length: > 0 } && assign is not null)
+        {
+            assign(value.Split("/")[^1]);
+        }
+    }
+}
---
+++ GithubAction/Program.cs
@@ -0,0 +1,36 @@
+﻿using GithubAction;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using CommandLine;
+using Microsoft.Extensions.Logging;
+using System;
+using static CommandLine.Parser;
+using System.Linq;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+   .Build();
+
+static TService Get<TService>(IHost host)
+    where TService : notnull =>
+    host.Services.GetRequiredService<TService>();
+
+static async Task PurgeBranchesAsync(ActionInputs inputs, IHost host) 
+{
+    Environment.Exit(0);
+}
+
+var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+parser.WithNotParsed(
+    errors =>
+    {
+        Get<ILoggerFactory>(host)
+            .CreateLogger("PurgeGitBranches.Program")
+            .LogError(
+                string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
+
+        Environment.Exit(2);
+    });
+
+await parser.WithParsedAsync(options => PurgeBranchesAsync(options, host));
+await host.RunAsync();
